877c991a4d37ccd7ca138a160fcf97a39660c452
Toshi <huynhduykhoi619@gmail.com>
2023-02-22 21:49:15 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-//const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contactsRouter = require("./app/routes/route");
@@ -13,14 +13,14 @@ app.get("/", (req, res) => {
 	res.json({ message: "Welcome to contact book application." });
 });
 
-// app.use((req, res, next) => {
-// 	return next(new ApiError(404, "Resource not found"));
-// });
+app.use((req, res, next) => {
+	return next(new ApiError(404, "Resource not found"));
+});
 
-// app.use((err, req, res, next) => {
-// 	return res
-// 		.status(error.statusCode || 500)
-// 		.json({ message: error.message || "Internal Server Error" });
-// });
+app.use((err, req, res, next) => {
+	return res
+		.status(err.statusCode || 500)
+		.json({ message: err.message || "Internal Server Error" });
+});
 
 module.exports = app;
