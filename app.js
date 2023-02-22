af922acf0038aba532308940079f8703c474d498
Toshi <huynhduykhoi619@gmail.com>
2023-02-22 21:42:19 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 //const ApiError = require("./app/api-error");
 
 const app = express();
-//const contactsRouter = require("./app/routes/route");
+const contactsRouter = require("./app/routes/route");
 
 app.use(cors());
 app.use(express.json());
-//app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to contact book application." });
